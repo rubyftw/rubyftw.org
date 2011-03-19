eb6f0e039b2163b9e747bb976d8870439ecdd88d
Phillip Ridlen <phillip@ridlen.net>
2011-03-18 23:41:25 -0500

    remove extra </a>

--- helpers.rb
+++ helpers.rb
@@ -56,7 +56,7 @@ module FrankHelpers
     html = %Q(<ul class="members">\n)
     HTTParty.get('http://github.com/api/v2/json/repos/show/rubyftw/rubyftw.org/watchers?full=1').parsed_response["watchers"].sort{|w,x| w["login"] <=> x["login"] }.each do |member|
       unless member["login"] == 'rubyftw'
-        html << "\t<li>#{github_link gravatar(member["gravatar_id"], 48, :alt => member["login"], :title => member["name"]), :user => member["login"]}</a></li>\n"
+        html << "\t<li>#{github_link gravatar(member["gravatar_id"], 48, :alt => member["login"], :title => member["name"]), :user => member["login"]}</li>\n"
       end
     end
     html << %Q(</ul>\n)
