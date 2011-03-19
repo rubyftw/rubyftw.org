04dcba2051ea0900b3810c6b26250f569fe09f30
Phillip Ridlen <phillip@ridlen.net>
2011-03-18 23:44:10 -0500

    remove inline styles on 'fork me'

--- helpers.rb
+++ helpers.rb
@@ -49,7 +49,7 @@ module FrankHelpers
   end
 
   def fork_me
-    github_link %Q(<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://d3nwyuy0nl342s.cloudfront.net/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub">)
+    github_link %Q(<img id="fork_me" src="https://d3nwyuy0nl342s.cloudfront.net/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub">)
   end
 
   def member_list
