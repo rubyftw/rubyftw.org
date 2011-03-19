413e4000ee7c7b71aa39298c80697be0f83fcf0d
Phillip Ridlen <phillip@ridlen.net>
2011-03-18 23:40:33 -0500

    add space between attributes on gravatar

--- helpers.rb
+++ helpers.rb
@@ -65,7 +65,7 @@ module FrankHelpers
   def gravatar(id, size = 48, html_options = {})
     img = %Q(<img src="https://secure.gravatar.com/avatar/#{id}?s=#{size}")
     html_options.each do |attr|
-      img << %Q(#{attr[0].to_s}="#{attr[1].to_s}")
+      img << %Q( #{attr[0].to_s}="#{attr[1].to_s}")
     end
     img << " />"
   end
