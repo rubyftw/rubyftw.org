bc64df60d747db460a59b3fe70cd4024da9659e7
Phillip Ridlen <phillip@ridlen.net>
2011-03-21 17:42:07 -0500

    separate layout and content

--- app/app.rb
+++ app/app.rb
@@ -2,18 +2,30 @@ require 'app/helpers/all'
 
 module RubyFtw
   class App < Sinatra::Base
-    set :views, File.dirname(__FILE__) + '/views'
+    set :views, ['app/layout','app/content']
     
-    get '/' do
-      erb :index
-    end
-
     get '/css/:stylesheet.css' do
       sass "css/#{params[:stylesheet]}".to_sym
     end
     
+    get '*' do
+      path = params['splat'].to_s.sub(/^\//,'').sub(/\/$/,'')      
+      if File.exists?("#{File.dirname(__FILE__)}/content/#{path}.erb")
+        erb :"#{path}"
+      elsif File.exists?("#{File.dirname(__FILE__)}/content/#{path}/index.erb")
+        erb :"#{path}/index"
+      else
+        raise Sinatra::NotFound
+      end
+    end
+    
     helpers do
       include Helpers
+      
+      def find_template(views, name, engine, &block)
+        Array(views).each {|v| super(v, name, engine, &block) }
+      end
     end
+    
   end
 end
