414477db819d2c6b40f38799982441b93cc8a2e3
Jake Good <jake@developstuff.com>
2012-06-12 20:11:24 -0500

    Change to V3 API

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -3,9 +3,9 @@ module RubyFtw
     module GithubHelper
       def member_list(portrait_size = 48)
         html = %Q(<ul class="members">\n)
-        HTTParty.get('http://github.com/api/v2/json/repos/show/rubyftw/rubyftw.org/watchers?full=1').parsed_response["watchers"].sort{|w,x| w["login"] <=> x["login"] }.each do |member|
+        HTTParty.get('https://api.github.com/repos/rubyftw/rubyftw.org/watchers').parsed_response["watchers"].sort{|w,x| w["login"] <=> x["login"] }.each do |member|
           unless member["login"] == 'rubyftw'
-            html << "\t<li>#{github_link gravatar(member["gravatar_id"], portrait_size, :alt => member["login"], :title => member["name"]), :user => member["login"]}</li>\n"
+            html << "\t<li>#{github_link gravatar(member["gravatar_id"], portrait_size, :alt => member["login"], :title => member["login"]), :user => member["login"]}</li>\n"
           end
         end
         html << %Q(</ul>\n)
