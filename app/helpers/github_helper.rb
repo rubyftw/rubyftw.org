9418fc372e0063316b8e4122822e013092ddd3b4
Jake Good <jake@developstuff.com>
2012-07-13 18:46:17 -0500

    Changed from watchers to members

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -3,7 +3,7 @@ module RubyFtw
     module GithubHelper
       def member_list(portrait_size = 48)
         html = %Q(<ul class="members">\n)
-        HTTParty.get('https://api.github.com/repos/rubyftw/rubyftw.org/watchers').parsed_response.sort{|w,x| w["login"] <=> x["login"] }.each do |member|
+        HTTParty.get('https://api.github.com/orgs/rubyftw/members').parsed_response.sort{|w,x| w["login"] <=> x["login"] }.each do |member|
           unless member["login"] == 'rubyftw'
             html << "\t<li>#{github_link gravatar(member["gravatar_id"], portrait_size, :alt => member["login"], :title => member["login"]), :user => member["login"]}</li>\n"
           end
